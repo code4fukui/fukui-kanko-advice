363d2d52a9e9f17da22bab1fea60f6185be3045b
Taisuke Fukuno <fukuno@jig.jp>
2025-09-18 14:05:49 +0900

    update data

--- make.js
+++ make.js
@@ -3,24 +3,27 @@ import { fetchKankoAdvice } from "./fetchKankoAdvice.js";
 import { Day } from "https://js.sabae.cc/DateTime.js";
 
 //const offset = 6; // run on Tuesday
-const offset = 5; // run on Monday
+//const offset = 5; // run on Monday
 //const offset = 4; // run on Sunday
 //const offset = 3; // run on Saturday
 //const offset = 2; // run on Friday
-//const offset = 1; // run on Thusday
+const offset = 1; // run on Thusday
 //const offset = 0; // run on Wednesday
-//const offset = undefined;
+//const offset = undefined; // for startday && endday
+
+//const days = 7;
+const days = 14;
 
 // use if offset == undefnied
-const startday = "2025-04-26";
-const endday = "2025-05-06";
+const startday = "2025-09-01";
+const endday = "2025-09-17";
 
 const areas0 = await CSV.fetchJSON("https://code4fukui.github.io/fukui-kanko-survey/area.csv");
 const areas = areas0.filter(a => a.通し番号).sort((a, b) => a.通し番号 - b.通し番号);
 //console.log(areas);
 
 const data0 = await CSV.fetchJSON("https://code4fukui.github.io/fukui-kanko-survey/all.csv");
-const start = offset !== undefined ? new Day().dayBefore(7 + offset) : new Day(startday);
+const start = offset !== undefined ? new Day().dayBefore(days + offset) : new Day(startday); // 1週間 or 2週間
 const end = offset !== undefined ? new Day().dayBefore(1 + offset).toString() : new Day(endday);
 
 let list = [];
