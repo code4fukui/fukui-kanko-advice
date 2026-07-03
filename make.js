6786c36480bd27964163e4d049e9d9b4723bfcd7
Taisuke Fukuno <fukuno@jig.jp>
2026-07-03 22:19:21 +0900

    update data

--- make.js
+++ make.js
@@ -15,8 +15,8 @@ const offset = undefined; // for startday && endday
 const days = 14;
 
 // use if offset == undefnied
-const startday = "2026-05-01";
-const endday = "2026-05-31";
+const startday = "2026-06-01";
+const endday = "2026-06-30";
 
 const areas0 = await CSV.fetchJSON("https://code4fukui.github.io/fukui-kanko-survey/area.csv");
 const areas = areas0.filter(a => a.通し番号).sort((a, b) => a.通し番号 - b.通し番号);
